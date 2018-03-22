64f4bc8a569934dcad615de6d72b92c36cb098fa
JessieYY <sijiey2@uci.edu>
2018-03-22 10:46:11 -0700

    Create Solution.java

---
+++ BackTracking/784.LetterCasePermutation/Solution.java
@@ -0,0 +1,43 @@
+// DFS (Recursion)
+// Key words: Return a list of all possible strings
+// Idea: for each char, we either make it upper case or lower case
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Solution1 {
+	List<String> result = new ArrayList<>();
+
+	public List<String> letterCasePermutation(String S) {
+		if (S == null) {
+			return result;
+		}
+
+		explore(S, 0);
+		return result;
+	}
+
+	public void explore(String S, int i) {
+		if (i == S.length()) {
+			result.add(S);
+			return;
+		}
+
+		if (Character.isDigit(S.charAt(i))) {
+			explore(S, i + 1);
+			return;
+		}
+		char[] chrs = S.toCharArray();
+		chrs[i] = Character.toLowerCase(chrs[i]);
+		explore(String.valueOf(chrs), i + 1);
+
+		chrs[i] = Character.toUpperCase(chrs[i]);
+		explore(String.valueOf(chrs), i + 1);
+	}
+
+	public static void main(String[] args) {
+		Solution1 sol = new Solution1();
+		sol.letterCasePermutation("a1b2");
+	}
+}
