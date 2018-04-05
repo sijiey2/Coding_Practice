c1358397e37e5b2983d0b0822cfd5f4e6e79c7e5
JessieYY <sijiey2@uci.edu>
2018-04-05 11:14:45 -0700

    Update Solution.java

--- LinkedList/109.ConvertSortedListtoBinarySearchTree/Solution.java
+++ LinkedList/109.ConvertSortedListtoBinarySearchTree/Solution.java
@@ -17,31 +17,35 @@ class TreeNode {
 
 public class Solution {
 	public TreeNode sortedListToBST(ListNode head) {
-		if (head == null) return null;
-		if (head.next == null) return new TreeNode(head.val);
-		if (head.next.next == null) {
-			TreeNode root = new TreeNode(head.next.val);
-			root.left = new TreeNode(head.val);
-			return root;
-		}
-		ListNode slow = head;
-		ListNode fast = head;
-		ListNode prev = slow; // attention (track prev of slow)
-
-		// get middle point
-		while (fast.next != null && fast.next.next != null) {
-			prev = slow;
-			slow = slow.next;
-			fast = fast.next.next;
-		}
-
-		// create tree (root/left/right)
-		TreeNode root = new TreeNode(slow.val);
-		ListNode right = slow.next;
-		prev.next = null; // instead of slow.next = null
-		root.left = sortedListToBST(head);
-		root.right = sortedListToBST(right);
-		return root;
+		if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            return new TreeNode(head.val);
+        }
+        if (head.next.next == null) {
+            TreeNode root = new TreeNode(head.next.val);
+            root.left = new TreeNode(head.val);
+            return root;
+        }
+        ListNode walker = head;
+        ListNode runner = head;
+        ListNode prev = walker;
+        
+        // get mid
+        while (runner.next != null && runner.next.next != null) {
+            prev = walker;
+            walker = walker.next;
+            runner = runner.next.next;
+        }
+        
+        // create tree
+        TreeNode root = new TreeNode(walker.val);
+        ListNode right = walker.next;
+        prev.next = null;
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(right);
+        return root;
 	}
 
 	public static void main(String[] args) {
